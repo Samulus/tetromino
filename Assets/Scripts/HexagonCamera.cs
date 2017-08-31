780bc526d6054d265e4dbddb3305f14e161be883
Samuel Vargas <savarga1@asu.edu>
2017-08-31 11:00:56 -0700

    [Mess] Rebase this later

---
+++ Assets/Scripts/HexagonCamera.cs
@@ -0,0 +1,22 @@
+﻿/*
+	HexagonCamera.cs
+	Author: Samuel Vargas
+	
+	This camera rotates in increments of 45
+	degrees slowly over time.
+*/
+
+using UnityEngine;
+
+public class HexagonCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
