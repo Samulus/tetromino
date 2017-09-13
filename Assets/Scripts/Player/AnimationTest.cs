2337c722819f3a5c1cc1dab6e092c9c07abdccc7
Samuel <savarga1@asu.edu>
2017-09-13 15:50:33 -0700

    Add simplistic cliff detection

--- Assets/Scripts/Player/AnimationTest.cs
+++ Assets/Scripts/Player/AnimationTest.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AnimationTest : MonoBehaviour {
   
   private Animation _animation;
   private CharacterController _characterController;
+  private CliffEdgeDetection _cliffEdgeDetection;
 
-  void Start() {
+  void Awake() {
     _animation = GetComponent<Animation>();
     _characterController = GetComponent<CharacterController>();
+    _cliffEdgeDetection = GetComponent<CliffEdgeDetection>();
+    
+    Debug.Assert(_cliffEdgeDetection != null);
   }
-  
+
   void Update() {
+
     // Rotations
     if (Input.GetKeyDown("d")) {
       transform.Rotate(Vector3.up, 90);
@@ -21,21 +24,20 @@ public class AnimationTest : MonoBehaviour {
     if (Input.GetKeyDown("a")) {
       transform.Rotate(Vector3.up, -90);
     }
-    
+
     if (Input.GetKey("e")) {
       Debug.Log("PickUp");
       _animation.Play("PickUp", PlayMode.StopAll);
       return;
     }
 
-    if (Input.GetKey("space")) {
+    if (!_cliffEdgeDetection.IsFacingCliff() && Input.GetKey("space")) {
       _animation.Play("Walk", PlayMode.StopAll);
       Vector3 forward = transform.TransformDirection(Vector3.forward);
       _characterController.SimpleMove(forward);
     }
     else {
       _animation.Play("Idle", PlayMode.StopAll);
-
     }
   }
 }
---
+++ Assets/Scripts/Player/CliffEdgeDetection.cs
@@ -0,0 +1,21 @@
+﻿/*
+	CliffEdgeDetection.cs
+	Author: Samuel Vargas
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR.WSA;
+
+public class CliffEdgeDetection : MonoBehaviour {
+	
+	public bool IsFacingCliff() {
+		Vector3 start = transform.position + (transform.TransformDirection(Vector3.forward) / 4.0f);
+		bool isCliff = !Physics.Raycast(start, Vector3.down, 1.0f);
+    Debug.DrawRay(start, Vector3.down, isCliff ? Color.red : Color.blue);
+		return isCliff;
+	}
+	
+}
