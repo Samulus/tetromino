3c2bff441f4cd300c7db67e61b535ff3b5c3670f
Samuel Vargas <savarga1@asu.edu>
2017-09-30 20:22:18 -0700

    Improve control scheme

--- Assets/Entities/Player/Movement.cs
+++ Assets/Entities/Player/Movement.cs
@@ -1,52 +1,46 @@
 ﻿using UnityEngine;
 
 namespace Player {
-  public class Movement : MonoBehaviour {
-    private Animation _animation;
-    private CharacterController _characterController;
-    private State _state;
-    private CliffDetect _cliffDetect;
 
-    private void Awake() {
-      _animation = GetComponent<Animation>();
-      _characterController = GetComponent<CharacterController>();
-      _cliffDetect = GetComponent<CliffDetect>();
-      _state = GetComponent<State>();
+    public class Movement : MonoBehaviour {
+        private Animation _animation;
+        private CharacterController _characterController;
+        private State _state;
+        private CliffDetect _cliffDetect;
+
+        private void Awake() {
+            _animation = GetComponent<Animation>();
+            _characterController = GetComponent<CharacterController>();
+            _cliffDetect = GetComponent<CliffDetect>();
+            _state = GetComponent<State>();
+        }
+
+        private void Start() {
+            _animation.Play("Idle", PlayMode.StopAll);
+        }
+
+        private void Update() {
+            // Avoid moving if our instance isn't active.
+            if (!_state.IsActive()) {
+                return;
+            }
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 200.0f;
+            var z = Input.GetAxis("Vertical") * Time.deltaTime;
+            if (x != 0.0f) {
+                transform.Rotate(0, x, 0);
+            }
+            if (z > 0.0f) {
+                _animation.Play("Walk", PlayMode.StopAll);
+                if (!_cliffDetect.IsCliffInfront()) {
+                    var forward = transform.TransformDirection(Vector3.forward);
+                    _characterController.SimpleMove(forward * 1.5f);
+                }
+            }
+            if (x == 0.0f && z == 0.0f) {
+                _animation.Play("Idle", PlayMode.StopAll);
+            }
+        }
+
     }
 
-    private void Start() {
-      _animation.Play("Idle", PlayMode.StopAll);
-    }
-
-    private void Update() {
-      // Avoid moving if our instance isn't active.
-      if (!_state.IsActive()) {
-        return;
-      }
-
-      // Left / Right Rotations
-      if (Input.GetKeyDown("d")) {
-        transform.Rotate(Vector3.up, 90);
-      }
-
-      if (Input.GetKeyDown("a")) {
-        transform.Rotate(Vector3.up, -90);
-      }
-
-      // Allow the robots to walk forward and backwards.
-      if (Input.GetKey("w") && !_cliffDetect.IsCliffInfront()) {
-        _animation.Play("Walk", PlayMode.StopAll);
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        _characterController.SimpleMove(forward);
-      }
-      else if (Input.GetKey("s") && !_cliffDetect.IsCliffBehind()) {
-        _animation.Play("Walk", PlayMode.StopAll); // TODO: Backpedaling animation.
-        Vector3 forward = transform.TransformDirection(Vector3.back);
-        _characterController.SimpleMove(forward);
-      }
-      else {
-        _animation.Play("Idle", PlayMode.StopAll);
-      }
-    }
-  }
 }
