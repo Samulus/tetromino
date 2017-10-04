554bc1fffbbf81a9264aa8e595d0bafcded18898
Samuel <savarga1@asu.edu>
2017-10-04 13:20:13 -0700

    Add some C# animation stuff

--- Assets/Entities/Player/States/Idling.cs
+++ Assets/Entities/Player/States/Idling.cs
@@ -14,22 +14,34 @@ using UnityEngine;
 namespace Entities.Player.States {
   public class Idling : MonoBehaviour {
     private FiniteStateMachine _finiteStateMachine;
+    private CharacterController _characterController;
     private Animation _animation;
+    private bool rotateRight;
 
     private void Start() {
       _finiteStateMachine = GetComponentInParent<FiniteStateMachine>();
       _animation = GetComponentInParent<Animation>();
-      _animation.Play("Walk", PlayMode.StopAll);
-
-      Animation anim = _animation;
-      foreach(AnimationState state in anim)
-      {
-        Debug.Log(state.name);
-      }
+      _characterController = GetComponentInParent<CharacterController>();
     }
 
     private void Update() {
       if (!_finiteStateMachine.IsActive(this)) return;
+
+      var x = Input.GetAxis("Horizontal") * Time.deltaTime * 200.0f;
+      var z = Input.GetAxis("Vertical") * Time.deltaTime;
+      if (x > 0.0f && z == 0.0f) {
+        // TODO: Potential Floating Point Error _animation
+        if (!_animation.isPlaying) {
+          rotateRight = true;
+          _animation.Play("RotateRight", PlayMode.StopAll);
+        }
+      }
+
+      if (rotateRight && !_animation.isPlaying) {
+        transform.parent.parent.Rotate(0, 90f, 0);
+        rotateRight = false;
+      }
     }
   }
+  
 }
