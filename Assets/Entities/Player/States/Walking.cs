37645c9cc19cec755387ecbbc6790410e43cddf1
Samuel <savarga1@asu.edu>
2017-10-05 13:03:12 -0700

    Try to use CrossFade, FSM improvements

--- Assets/Entities/Player/States/Walking.cs
+++ Assets/Entities/Player/States/Walking.cs
@@ -3,18 +3,48 @@
     Author: Samuel Vargas
 */
 
+using Player;
 using UnityEngine;
 
 namespace Entities.Player.States {
-  public class Walking : MonoBehaviour {
+  public class Walking : FiniteStateMonoBehaviour {
     private FiniteStateMachine _finiteStateMachine;
+    private Animation _animation;
+    private CharacterController _characterController;
+    private CliffDetect _cliffDetect;
+
+    public override void Enter() {
+    }
+
+    public override void Exit() {
+    }
 
     private void Start() {
       _finiteStateMachine = GetComponentInParent<FiniteStateMachine>();
+      _animation = GetComponentInParent<Animation>();
+      _characterController = GetComponentInParent<CharacterController>();
+      _cliffDetect = GetComponentInParent<CliffDetect>();
     }
 
     private void Update() {
       if (!_finiteStateMachine.IsActive(this)) return;
+      var x = Input.GetAxis("Horizontal") * Time.deltaTime * 200.0f;
+      var z = Input.GetAxis("Vertical") * Time.deltaTime;
+
+      // Transfer back to Idling if the user has stopped walking.
+      if (Mathf.Abs(x) < float.Epsilon &&
+          Mathf.Abs(z) < float.Epsilon) {
+        _finiteStateMachine.ChangeState(typeof(Idling).Name);
+        return;
+      }
+
+      transform.parent.parent.Rotate(0, x, 0);
+
+      if (!_cliffDetect.IsCliffInfront()) {
+        var forward = transform.TransformDirection(Vector3.forward);
+        _characterController.SimpleMove(forward * 1.5f);
+        _animation.CrossFade("Walk");
+      }
     }
   }
 }
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,12 +7,15 @@ public class CameraController : MonoBehaviour {
   private Vector3 _eye;
 
   void Start() {
+    /*
     _eye = Player.transform.position;
     _eye.y -= EyeHeight;
     _eye = Vector3.zero;
+    */
   }
 
   void Update() {
+    /*
     if (Input.GetKey("q")) {
       transform.RotateAround(_eye, Vector3.up, Time.deltaTime * SpeedMetersPerSec);
     }
@@ -20,5 +23,6 @@ public class CameraController : MonoBehaviour {
     else if (Input.GetKey("e")) {
       transform.RotateAround(_eye, Vector3.up, -Time.deltaTime * SpeedMetersPerSec);
     }
+    */
   }
 }
