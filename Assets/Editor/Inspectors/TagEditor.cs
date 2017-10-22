9cafa8f289da79030e52e4154d6eba10a1687382
Samuel Vargas <savarga1@asu.edu>
2017-10-21 18:46:15 -0700

    Swich to use PropertyEditor

--- Assets/Editor/Inspectors/TagEditor.cs
+++ Assets/Editor/Inspectors/TagEditor.cs
@@ -3,28 +3,20 @@
  * Author: Samuel Vargas
  */
 
-using System;
 using Tags;
 using UnityEditor;
-using UnityEngine;
 
 namespace Editor.Inspectors {
 
   [CustomEditor(typeof(Tag))]
-  public sealed class TagEditor : UnityEditor.Editor {
-    [SerializeField] private SerializedProperty _serialType;
-    [SerializeField] private SerializedProperty _serialSensorId;
-    [SerializeField] private SerializedProperty _serialAgentId;
-    [SerializeField] private SerializedProperty _serialGeometryId;
-    [SerializeField] private SerializedProperty _serialDeviceId;
+  public class TagEditor : UnityEditor.Editor {
+    private SerializedProperty _serialType;
+    private SerializedProperty _serialSensorId;
+    private SerializedProperty _serialAgentId;
+    private SerializedProperty _serialGeometryId;
+    private SerializedProperty _serialDeviceId;
 
-    [SerializeField] private TagType _selectedType;
-    [SerializeField] private SensorId _selectedSensorId;
-    [SerializeField] private AgentId _selectedAgentId;
-    [SerializeField] private GeometryId _selectedGeometryId;
-    [SerializeField] private DeviceId _selectedDeviceId;
-
-    private void OnEnable() {
+    private void SetupValues() {
       _serialType = serializedObject.FindProperty("Type");
       _serialSensorId = serializedObject.FindProperty("SensorId");
       _serialAgentId = serializedObject.FindProperty("AgentId");
@@ -33,51 +25,24 @@ namespace Editor.Inspectors {
     }
 
     public override void OnInspectorGUI() {
+      SetupValues();
       serializedObject.Update();
-
-      var objectType = ShowObjectTypeMenu();
-      EditorGUI.BeginChangeCheck();
-
-      switch (objectType) {
+      EditorGUILayout.PropertyField(_serialType);
+      switch ((TagType) _serialType.intValue) {
         case TagType.Agents:
-          _selectedAgentId = (AgentId) EditorGUILayout.EnumPopup("Agent", _selectedAgentId);
-          if (EditorGUI.EndChangeCheck()) {
-            _serialAgentId.intValue = (int) _selectedAgentId;
-          }
+          EditorGUILayout.PropertyField(_serialAgentId);
           break;
         case TagType.Geometry:
-          _selectedGeometryId = (GeometryId) EditorGUILayout.EnumPopup("Geometry", _selectedGeometryId);
-          if (EditorGUI.EndChangeCheck()) {
-            _serialGeometryId.intValue = (int) _selectedGeometryId;
-          }
+          EditorGUILayout.PropertyField(_serialGeometryId);
           break;
         case TagType.Device:
-          _selectedDeviceId = (DeviceId) EditorGUILayout.EnumPopup("Device", _selectedDeviceId);
-          if (EditorGUI.EndChangeCheck()) {
-            _serialDeviceId.intValue = (int) _selectedDeviceId;
-          }
+          EditorGUILayout.PropertyField(_serialDeviceId);
           break;
         case TagType.Sensor:
-          _selectedSensorId = (SensorId) EditorGUILayout.EnumPopup("Sensor", _selectedSensorId);
-          if (EditorGUI.EndChangeCheck()) {
-            _serialSensorId.intValue = (int) _selectedSensorId;
-          }
+          EditorGUILayout.PropertyField(_serialSensorId);
           break;
-        default:
-          throw new ArgumentOutOfRangeException();
       }
-
       serializedObject.ApplyModifiedProperties();
     }
-
-    private TagType ShowObjectTypeMenu() {
-      EditorGUI.BeginChangeCheck();
-      _selectedType = (TagType) EditorGUILayout.EnumPopup("Type", _selectedType);
-      if (EditorGUI.EndChangeCheck()) {
-        _serialType.intValue = (int) _selectedType;
-      }
-      return _selectedType;
-    }
   }
-
 }
