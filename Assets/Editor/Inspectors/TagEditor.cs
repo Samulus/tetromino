965eeea049b5b962195074629e92e7e0eb6caab6
Samuel Vargas <savarga1@asu.edu>
2017-10-21 17:14:37 -0700

    Revamp Tag system
    I included the TagEnum.cs, TagEditor.cs, and Tag.cs modules.
    
    Tags now have a custom editor that serialzies changes, this
    lets us avoid a gigantic dropdown menu of every enum in the
    game and allows us to differentiate between objects on a
    type / id basis.

---
+++ Assets/Editor/Inspectors/TagEditor.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * TagEditor.cs
+ * Author: Samuel Vargas
+ */
+
+using System;
+using System.Collections.Generic;
+using TagEnums;
+using Tags;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.Inspectors {
+
+  [CustomEditor(typeof(Tag))]
+  public sealed class TagEditor : UnityEditor.Editor {
+    [SerializeField] private SerializedProperty _serialType;
+    [SerializeField] private SerializedProperty _serialObjectId;
+    private TagType _tagType;
+    private TagId _tagId;
+    private int _tagIdMenuIndex;
+
+    private Dictionary<TagType, string[]> _objectIdDictionary;
+    private Dictionary<TagType, int[]> _objectValueDictionary;
+
+    public TagEditor() {
+      _objectIdDictionary = new Dictionary<TagType, string[]>();
+      _objectValueDictionary = new Dictionary<TagType, int[]>();
+      
+    }
+
+    private void Awake() {
+
+      foreach (TagType tagType in Enum.GetValues(typeof(TagType))) {
+        var arr = TagMethods.GetEnumsInRange((int) tagType, (int) tagType + 100);
+        _objectValueDictionary.Add(tagType, TagMethods.ListToValueArray(arr));
+        _objectIdDictionary.Add(tagType, TagMethods.ListToStringArray(arr));
+      }
+      
+      SetupValues();
+    }
+    
+    private void SetupValues() {
+      _serialType = serializedObject.FindProperty("Type");
+      _serialObjectId = serializedObject.FindProperty("Id");
+      _tagType = (TagType) _serialType.intValue;
+      _tagId = (TagId) _serialObjectId.intValue;
+
+      // Reset to 0 if the serialized enums is no longer valid due to
+      // editing the enums.
+      if (!Enum.IsDefined(typeof(TagType), _tagType)) {
+        _tagType = 0;
+      }
+
+      if (!Enum.IsDefined(typeof(TagId), _tagId)) {
+        _tagId = 0;
+      }
+
+      // Set tagIdMenuIndex to the correct value
+      if (_objectValueDictionary.ContainsKey(_tagType)) {
+        var active = _objectValueDictionary[_tagType];
+        _tagIdMenuIndex = 0;
+        for (var i = 0; i < active.Length; ++i) {
+          if (active[i] == (int) _tagId) {
+            _tagIdMenuIndex = i;
+          }
+        }
+      }
+    }
+
+    public override void OnInspectorGUI() {
+      //DrawDefaultInspector();
+      serializedObject.Update();
+      ShowObjectTypeMenu();
+      ShowObjectIdMenu();
+      serializedObject.ApplyModifiedProperties();
+    }
+
+    private void ShowObjectTypeMenu() {
+      EditorGUI.BeginChangeCheck();
+      _tagType = (TagType) EditorGUILayout.EnumPopup("Type:", _tagType);
+      if (EditorGUI.EndChangeCheck()) {
+        _serialType.intValue = (int) _tagType;
+        _serialObjectId.intValue = _objectValueDictionary[_tagType][0];
+      }
+    }
+
+    private void ShowObjectIdMenu() {
+      EditorGUI.BeginChangeCheck();
+      if (_objectIdDictionary.ContainsKey(_tagType)) {
+        _tagIdMenuIndex = EditorGUILayout.Popup("Id: ", _tagIdMenuIndex, _objectIdDictionary[_tagType]);
+        if (EditorGUI.EndChangeCheck()) {
+          _serialObjectId.intValue = _objectValueDictionary[_tagType][_tagIdMenuIndex];
+        }
+      }
+    }
+  }
+
+}
---
+++ Assets/Tags/TagEnums.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * TagEnums.cs
+ * Author: Samuel Vargas
+ *
+ * Notes:
+ *   - The TagType value should match the first TagId value for that specific
+ *     section.
+ *
+ *    - Each TagType can have TagId(s) that correspond from [n -> n+99]
+ *
+ *    - A TagType requires at least 1 corresponding TagId with the same value.
+ *
+ *    - There are no duplicates.
+ * 
+ *  Objects will deserialize incorrectly if these expectations are not met.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace TagEnums {
+
+  [Serializable]
+  public enum TagType : int {
+    NoType = -1,
+    Agents = 100,
+    Geometry = 200,
+    Device = 300,
+    Sensor = 400,
+  }
+
+  [Serializable]
+  public enum TagId : int {
+    NoId = -1,
+
+    // Agents
+    Player = 100,
+
+    // Geometry
+    Cube = 200,
+    Stairs = 201,
+
+    // Devices
+    LaserSender = 300,
+    ColorChanger = 301,
+    Crate = 302,
+
+    // Sensors
+    ObstructionZone = 400,
+    ItemPickUpZone = 401,
+    LedgeDetectionSensor = 402,
+  }
+
+  public static class TagMethods {
+    public static List<TagId> GetEnumsInRange(int min, int max) {
+      var output = new List<TagId>();
+      for (var i = min; i < max; ++i) {
+        if (Enum.IsDefined(typeof(TagId), i)) {
+          output.Add((TagId) i);
+        }
+      }
+      return output;
+    }
+    
+    public static int[] ListToValueArray(List<TagId> list) {
+      var output = new int[list.Count];
+      for (var i = 0; i < list.Count; ++i) {
+        output[i] = (int) list[i];
+      }
+      return output;
+    }
+
+    public static string[] ListToStringArray(List<TagId> list) {
+      var output = new string[list.Count];
+      for (var i = 0; i < list.Count; ++i) {
+        output[i] = Enum.GetName(typeof(TagId), list[i]);
+      }
+      return output;
+    }
+  }
+
+}
