c22ad796e8cd1bc9b7844328bcf12b6bb982ec8b
Samuel Vargas <savarga1@asu.edu>
2017-11-07 16:31:03 -0700

    Modify XBotIdle.cs to allow the user to push a SokoBlock

--- Assets/Scripts/Player/Animation/Locomotion/XBotIdle.cs
+++ Assets/Scripts/Player/Animation/Locomotion/XBotIdle.cs
@@ -1,40 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Entities.Player.Inventory;
-using Entities.Player.Sensors;
+﻿using Devices.SokoBlock;
+using MultiPurpose;
 using UnityEngine;
 
-public class XBotIdle : StateMachineBehaviour {
-  
-  public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-    animator.SetBool("isIdle", true);
+namespace Player.Animation.Locomotion {
+
+  public class XBotIdle : StateMachineBehaviour {
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+      animator.SetBool("isIdle", true);
+    }
+
+    public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+      var x = Input.GetAxis("Horizontal") * Time.deltaTime * 250.0f;
+      animator.transform.root.Rotate(0, x, 0);
+
+      var itemPrescenceZone = GameObject.Find("Player/Sensor/ItemPushZone").GetComponent<ItemPrescenceZone>();
+      var obstructionPrescenceZone =
+        GameObject.Find("Player/Sensor/ObstructionPushZone").GetComponent<ItemPrescenceZone>();
+
+
+      // Check if we can push a SokoBlock
+      if (Input.GetKey("space")) {
+        Debug.Log("Space");
+        var obstructionPresent = obstructionPrescenceZone.GetItem() != null;
+        var itemPresent = itemPrescenceZone.GetItem() != null;
+
+        if (!obstructionPresent && itemPresent) {
+          itemPrescenceZone.GetItem().GetComponent<SokoBlockPusher>().Push();
+        }
+      }
+
+      // Move forward
+      if (Input.GetKeyDown("w")) {
+        animator.SetBool("isIdle", false);
+        animator.SetBool("isWalking", true);
+      }
+    }
   }
-  
-  public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-    var x = Input.GetAxis("Horizontal") * Time.deltaTime * 250.0f;
-    animator.transform.root.Rotate(0, x, 0);
 
-    //var itemPickupZone = animator.transform.root.GetComponentInChildren<ItemPickupZone>();
-    //var obstructionPickupZone = animator.transform.root.GetComponentInChildren<ObstructionPickupZone>();
-    var inventory = animator.transform.root.GetComponentInChildren<Inventory>();
-    
-    // Drop Items
-    if (Input.GetKeyDown(KeyCode.Space) && inventory.HasItem()) {
-      inventory.DropItem();
-    }
-
-    // Pick Up items
-    /*
-    else if (Input.GetKeyDown(KeyCode.Space) && !obstructionPickupZone.IsObstructionPresent() &&
-        itemPickupZone.IsPickUpPresent()) {
-      inventory.AddItem(itemPickupZone.GetPickUp());
-    }
-    */
-
-    // Move forward
-    if (Input.GetKeyDown("w")) {
-      animator.SetBool("isIdle", false);
-      animator.SetBool("isWalking", true);
-    }
-  }
 }
