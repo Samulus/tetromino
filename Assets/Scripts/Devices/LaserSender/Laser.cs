210389e37d94d5a50737eac8361c754c96961294
Samuel Vargas <savarga1@asu.edu>
2017-11-06 18:04:29 -0700

    Use transform.forward solution for proper BoxCollider recalculations

--- Assets/Scripts/Devices/LaserSender/Laser.cs
+++ Assets/Scripts/Devices/LaserSender/Laser.cs
@@ -7,59 +7,70 @@
  */
 
 using System;
-using Colors;
 using UnityEngine;
-using UnityEngine.Rendering;
-using Util;
 using Tags;
 
 namespace Devices.LaserSender {
 
+  internal enum Direction {
+    PosZ,
+    NegZ,
+    PosX,
+    NegX
+  }
+
   public class Laser : MonoBehaviour {
-    private const float Width = 0.25f;
     private const float MaxDistance = 100.0f;
-    private Vector3 _laserPseudoIndefiniteEnd;
     private BoxCollider _laserCollider;
+    private Direction _facing;
 
     private void Start() {
-      // Setup start and end of laser
-      _laserPseudoIndefiniteEnd =
-        new Vector3(transform.position.x, transform.position.y, transform.position.z - MaxDistance);
-
       // Laser Collisions
       _laserCollider = gameObject.AddComponent<BoxCollider>();
       _laserCollider.isTrigger = true;
       _laserCollider.center = new Vector3(0, 0, MaxDistance / 2.0f);
       _laserCollider.size = new Vector3(transform.localScale.x / 2.0f, transform.localScale.y / 2.0f,
         transform.localScale.z * MaxDistance + 0.5f);
+
+      SetDirection();
+    }
+
+    private void SetDirection() {
+      if (Math.Abs(transform.forward.z - -1.0) < 0.02) {
+        _facing = Direction.NegZ;
+      }
+      else if (Math.Abs(transform.forward.z - 1.0) < 0.02) {
+        _facing = Direction.PosZ;
+      }
+      else if (Math.Abs(transform.forward.x - -1.0) < 0.02) {
+        _facing = Direction.NegX;
+      }
+      else if (Math.Abs(transform.forward.x - 1.0) < 0.02) {
+        _facing = Direction.PosX;
+      }
+      else {
+        Debug.Assert(false,
+          string.Format("Parent LaserSender: '{0}' was not placed at a 90 degree angle.", transform.parent.name));
+      }
     }
 
     private void OnTriggerStay(Collider other) {
       var maybeLaser = other.transform.GetComponent<Tag>();
       if (maybeLaser && maybeLaser.Type == TagType.Device && maybeLaser.DeviceId == DeviceId.Laser) return;
-      var distance = Math.Abs((transform.position.z - other.transform.position.z));
-      Debug.Log(distance);
-      var endPoint = transform.position;
-      endPoint.z = other.transform.position.z;
-      /*
-       * Method:
-       *
-       *   Set the center 'Z' of the Box Component to somewhere BETWEEN the player and the origin
-       *
-       */
-      
-      /*
-       * Patterns:
-       *     Center: 1
-       *     Size: 2
-       *
-       *     Center: 2
-       *     Size: 4
-       */
 
-      var middle = (transform.position.z - other.transform.position.z) / 2.0f;
-      Debug.Log(middle);
-      _laserCollider.center = new Vector3(0, 0, middle); 
+      var middle = 0.0f;
+      if (_facing == Direction.PosZ || _facing == Direction.NegZ) {
+        middle = Math.Abs(transform.position.z - other.transform.position.z) / 2.0f;
+      }
+      else if (_facing == Direction.PosX || _facing == Direction.NegX) {
+        middle = Math.Abs(transform.position.x - other.transform.position.x) / 2.0f;
+      }
+      else {
+        Debug.Assert(false,
+          string.Format("Parent LaserSender: '{0}' was not placed at a 90 degree angle.", transform.parent.name));
+      }
+
+      _laserCollider.center = new Vector3(0, 0, middle);
       _laserCollider.size = new Vector3(transform.localScale.x / 2.0f, transform.localScale.y / 2.0f, middle * 2.0f);
     }
 
