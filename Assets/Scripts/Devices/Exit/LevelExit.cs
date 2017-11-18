d99dbed0aefae2045ddf6fc2e268f3872a16e82c
Samuel Vargas <savarga1@asu.edu>
2017-11-18 13:07:55 -0700

    Fix LevelExit.cs, Fix Level 002

--- Assets/Scripts/Devices/Exit/LevelExit.cs
+++ Assets/Scripts/Devices/Exit/LevelExit.cs
@@ -1,18 +1,27 @@
-﻿using System;
+﻿/*
+ * LevelExit.cs
+ * Author: Samuel Vargas
+ */
+
+using Tags;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelExit : MonoBehaviour {
-  private int _currentlevel = 0;
-  
-  void OnTriggerEnter(Collider other) {
-    if (other.name.ToLower().Equals("human")) {
+namespace Devices.Exit {
+
+  public class LevelExit : MonoBehaviour {
+    private int _currentlevel;
+
+    private void OnTriggerEnter(Collider other) {
+      var maybeTag = other.gameObject.GetComponent<Tag>();
+      if (maybeTag == null || maybeTag.Type != TagType.Agent || maybeTag.AgentId != AgentId.Player) return;
       _currentlevel += 1;
       SceneManager.LoadScene(_currentlevel.ToString("000"));
     }
+
+    private void Start() {
+      int.TryParse(SceneManager.GetActiveScene().name, out _currentlevel);
+    }
   }
-  
-  void Start() {
-    _currentlevel = Int32.Parse(SceneManager.GetActiveScene().name);
-  }
+
 }
