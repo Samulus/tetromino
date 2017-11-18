e75d424319d73a7ddfe288be973b096af5f682e5
Samuel Vargas <savarga1@asu.edu>
2017-11-18 12:21:43 -0700

    SokoBlockpushing is grid only now

---
+++ Assets/Scripts/Devices/SokoBlock/SokoBlockNeverRotate.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * SokoBlockNeverRotate.cs
+ * Author: Samuel Vargas
+ */
+
+using UnityEngine;
+
+namespace Devices.SokoBlock {
+
+  public class SokoBlockNeverRotate : MonoBehaviour {
+    private void Start() {
+    }
+
+    private void Update() {
+    }
+  }
+
+}
