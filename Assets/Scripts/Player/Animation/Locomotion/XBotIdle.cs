252408b481c2fa8f3be5fe683c5179226faedc08
Samuel Vargas <savarga1@asu.edu>
2017-11-20 11:26:31 -0700

    Prevent Player from moving SokoBlock if color doesn't match

--- Assets/Scripts/Player/Animation/Locomotion/XBotIdle.cs
+++ Assets/Scripts/Player/Animation/Locomotion/XBotIdle.cs
@@ -6,36 +6,53 @@
  * at all.
  */
 
-using Devices.SokoBlock;
 using UnityEngine;
+using Util;
 
 namespace Player.Animation.Locomotion {
 
   public class XBotIdle : StateMachineBehaviour {
     public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
       PlayerActions.MaybeRotate(ref animator);
-      
+
       if (PlayerInput.RequestToWalkForward(ref animator)) {
         animator.SetBool("isWalking", true);
         animator.SetBool("isIdle", false);
         return;
       }
-      
+
       GameObject sokoBlock;
       if (PlayerInput.RequestToPushSokoBlock(out sokoBlock)) {
+        
+        // Ensure the player is the correct color before we allow them 
+        // to interact with the SokoBlock
+        var theirs = sokoBlock.gameObject.GetComponent<GameObjectColor>();
+        var ours = animator.transform.GetComponentInChildren<GameObjectColor>();
+        if (theirs.Value != ours.Value) {
+          return;
+        }
+
         animator.SetBool("isPushing", true);
         animator.SetBool("isIdle", false);
         var inventory = GameObject.Find("Inventory").transform;
         sokoBlock.transform.SetParent(inventory.transform, true);
       }
-      
+
       else if (PlayerInput.RequestToPullSokoBlock(out sokoBlock)) {
+        
+        // Ensure the player is the correct color before we allow them 
+        // to interact with the SokoBlock
+        var theirs = sokoBlock.gameObject.GetComponent<GameObjectColor>();
+        var ours = animator.transform.GetComponentInChildren<GameObjectColor>();
+        if (theirs.Value != ours.Value) {
+          return;
+        }
+        
         animator.SetBool("isPulling", true);
         animator.SetBool("isIdle", false);
         var inventory = GameObject.Find("Inventory").transform;
         sokoBlock.transform.SetParent(inventory.transform, true);
       }
-
     }
   }
 
