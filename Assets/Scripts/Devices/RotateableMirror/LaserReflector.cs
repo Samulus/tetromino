f74fa3b0d65d968dae2a5312d44f418d449ff6bf
Samuel Vargas <savarga1@asu.edu>
2017-12-02 17:35:39 -0700

    Fix LaserReflector so it infinitely reflects now

--- Assets/Scripts/Devices/RotateableMirror/LaserReflector.cs
+++ Assets/Scripts/Devices/RotateableMirror/LaserReflector.cs
@@ -3,7 +3,6 @@
  * Author: Samuel Vargas
  */
 
-using Devices.Lasers;
 using Devices.LaserSender;
 using Tags;
 using UnityEngine;
@@ -15,7 +14,6 @@ namespace Devices.RotateableMirror {
     private Laser _laser;
     private Vector3 _reflectionDirection;
     private Collider _incomingLaser;
-    private ReverseRaycaster _reverseRaycaster;
 
     private void Start() {
       _laser = gameObject.GetComponentInChildren<Laser>();
@@ -26,26 +24,16 @@ namespace Devices.RotateableMirror {
       var maybeTag = other.GetComponent<Tag>();
       if (maybeTag == null || maybeTag.Type != TagType.Device || maybeTag.DeviceId != DeviceId.Laser) return false;
       if (maybeTag.Type == TagType.Sensor) return false;
-      if (other.name == "_origin") return false; // TODO: Ensure we aren't colliding with ourselves in a better way
-      if (other.name == "_normal") return false; // TODO: Ensure we aren't colliding with ourselves in a better way
+      //if (other.name == "_origin") return false; // TODO: Ensure we aren't colliding with ourselves in a better way
+      //if (other.name == "_normal") return false; // TODO: Ensure we aren't colliding with ourselves in a better way
       return true;
     }
 
     private void OnTriggerEnter(Collider other) {
-      if (ColliderIsALaser(other)) {
-        Debug.Log(gameObject + " collided with laser", this);
-      }
       // Avoid setting _incomingLaser if one already exists or if the object that triggered
       // the collision is not a laser.
-      if (_incomingLaser != null || !ColliderIsALaser(other)) return;
-
+      if (!ColliderIsALaser(other)) return;
       _incomingLaser = other;
-      var maybeTag = other.GetComponent<Tag>();
-      if (maybeTag == null || maybeTag.Type != TagType.Device || maybeTag.DeviceId != DeviceId.Laser) return;
-      if (maybeTag.Type == TagType.Sensor) return;
-      if (other.name == "_origin") return; // TODO: Ensure we aren't colliding with ourselves in a better way
-      if (other.name == "_normal") return; // TODO: Ensure we aren't colliding with ourselves in a better way
-
       var hit = other.GetComponent<LaserRaycaster>().ReverseHit(transform);
       if (hit.HasValue) {
         _laser.transform.position = hit.Value.point;
@@ -65,15 +53,10 @@ namespace Devices.RotateableMirror {
         _laser.transform.position = hit.Value.point;
         _laser.GetComponent<GameObjectColor>().Value = _incomingLaser.GetComponent<GameObjectColor>().Value;
       }
-      else {
-        Debug.Log(gameObject.name + " failed to find reverse collision.");
-      }
     }
 
     private void OnTriggerExit(Collider other) {
-      //if (_incomingLaser != other) return;
-      var maybeTag = other.GetComponent<Tag>();
-      if (maybeTag == null || maybeTag.Type != TagType.Device || maybeTag.DeviceId != DeviceId.Laser) return;
+      if (!ColliderIsALaser(other)) return;
       _incomingLaser = null;
       _laser.enabled = false;
     }
